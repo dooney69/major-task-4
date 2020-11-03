6c9e55903ccf21d0ba747f5b73b487e156e55f1b
dooney69 <65260475+dooney69@users.noreply.github.com>
2020-11-03 22:08:07 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function changeBg(color){
+    document.body.style.background = color;
+    if (background = '#0392BF'){
+        document.body.style.color = '#B3D4E0'
+    }
+   else if (background = '#ED48BA'){
+        document.body.style.color = '#B3D4E0'
+    }
+   else if (background = '#FBEC06'){
+        document.body.style.color = '#333333'
+    }
+   else if (background = '#B3D4E0'){
+        document.body.style.color = '#333333'
+    }
+}
